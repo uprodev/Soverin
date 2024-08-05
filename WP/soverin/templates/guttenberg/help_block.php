e3f811257933979159fa73a8caee825851068264
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-08-05 19:24:52 +0200

    Create help_block.php

---
+++ WP/soverin/templates/guttenberg/help_block.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+?>
+
+<section class="help">
+    <div class="content-width">
+        <div class="content">
+            <div class="help-menu">
+                <ul>
+                    <li class="current"><a href="#"><img src="img/icon-1-1.svg" alt="">Account</a></li>
+                    <li><a href="#"><img src="img/icon-1-2.svg" alt="">Clients</a></li>
+                    <li><a href="#"><img src="img/icon-1-3.svg" alt="">Domains</a></li>
+                    <li><a href="#"><img src="img/icon-1-4.svg" alt="">Email</a></li>
+                    <li><a href="#"><img src="img/icon-1-5.svg" alt="">FAQ</a></li>
+                    <li><a href="#"><img src="img/icon-1-6.svg" alt="">Get Started</a></li>
+                    <li><a href="#"><img src="img/icon-1-7.svg" alt="">Webmail</a></li>
+                </ul>
+            </div>
+
+            <div class="help-content">
+                <div class="account">
+                    <div class="title">
+                        <h2><img src="img/icon-1-1.svg" alt="">Account</h2>
+                        <p>Learn about your account and how to manage it.</p>
+                    </div>
+                    <ul class="account-list">
+                        <li><a href="#">Delete your account</a></li>
+                        <li><a href="#">logging in to your dashboard</a></li>
+                        <li><a href="#">change your password</a></li>
+                        <li><a href="#">reset your password</a></li>
+                        <li><a href="#">renew your account</a></li>
+                        <li><a href="#">adding a slot for a mailbox</a></li>
+                        <li><a href="#">change your billing address</a></li>
+                        <li><a href="#">changing the language setting</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
